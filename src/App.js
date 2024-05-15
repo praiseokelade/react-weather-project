18dc9145f8cbf7381bbf898bf0282d38daad331b
praiseokelade <151168193+praiseokelade@users.noreply.github.com>
2024-05-15 15:24:18 +0100

    Added GitHub repository

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather Application
+      <h1>Weather Application</h1>
+      <footer>
+        This project was coded by Praise Okelade and is open-sourced on{" "}
+        <a
+          href="https://github.com/praiseokelade/react-weather-project"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
