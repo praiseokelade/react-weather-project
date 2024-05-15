538d3d83aa23cceee346990dcecd5a3864afc47b
praiseokelade <151168193+praiseokelade@users.noreply.github.com>
2024-05-15 19:06:52 +0100

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was coded by Praise Okelade and is open-sourced on{" "}
-        <a
-          href="https://github.com/praiseokelade/react-weather-project"
-          target="_blank"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Praise Okelade and is open-sourced on{" "}
+          <a
+            href="https://github.com/praiseokelade/react-weather-project"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
